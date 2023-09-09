fff39c46e2186ef9c9b08958568e311597b85fdc
pkieber <113725311+pkieber@users.noreply.github.com>
2023-09-09 22:39:51 +0200

    import blog components in index.js

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const PostWidget = () => {
-  return (
-    <div>PostWidget</div>
-  )
+const PostWidget = () => {
+    return (
+        <div>PostWidget</div>
+    )
 }
+
+export default PostWidget
