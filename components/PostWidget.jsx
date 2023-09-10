7af4d399a03b419ea71610c7f78c5d63e1671997
pkieber <113725311+pkieber@users.noreply.github.com>
2023-09-10 14:07:25 +0200

    implement getRecentPosts and getSimilarPosts

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+import Link from 'next/link';
+import { getRecentPosts, getSimilarPosts } from '@/services';
 
-const PostWidget = () => {
+
+// In normal view, recent posts should be shown. 
+// Inside a post, related posts should be shown.
+// useEffect only changes when slug changes. 
+const PostWidget = ({ categories, slug }) => {
+    const [relatedPosts, setRelatedPosts] = useState([])
+
+    useEffect(() => {
+        if(slug) {
+            getSimilarPosts(categories, slug)
+                .then((result) => setRelatedPosts(result))
+        } else {
+            getRecentPosts()
+                .then((result) => setRelatedPosts(result))
+        }
+    }, [slug])
+    
+    console.log(relatedPosts);
+
     return (
-        <div>PostWidget</div>
+        <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
+            <h3>
+                { slug ? 'Related Posts' : 'Recent Posts'}
+            </h3>
+        </div>
     )
 }
 
--- services/index.js
+++ services/index.js
@@ -2,6 +2,7 @@ import { request, gql } from 'graphql-request';
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
+
 export const getPosts = async () => {
     const query = gql`
         query MyQuery {
@@ -40,13 +41,59 @@ export const getPosts = async () => {
         console.error('Error fetching posts:', error);
         return [];
     }
+};
 
-    /*
+
+export const getRecentPosts = async () => {
+    const query = gql`
+        query GetPostDetails() {
+            posts(
+                orderBy: createdAt_ASC
+                last: 3
+            ) {
+                title
+                featuredImage {
+                    url
+                }
+                createdAt
+                slug
+            }
+        }
+    `;
+
     try {
         const result = await request(graphqlAPI, query);
-        return result.postsConnection.edges.map(edge => edge.node);
+        return result.posts;
     } catch (error) {
-        console.error('Error fetching posts:', error);
+        console.error('Error fetching recent posts:', error);
         return [];
-    } */
+    }
+};
+
+
+// Similar posts --> excluding the current slug, but showing slugs within the same category. 
+export const getSimilarPosts = async () => {
+    const query = gql`
+        query GetPostDetails($slug: String!, $categories: [String!]) {
+            posts(
+                where: { slug_not: $slug, AND: {categories_some: { slug_in: $categories }}}
+                last: 3
+            ) {
+                title
+                featuredImage {
+                    url
+                }
+                createdAt
+                slug
+            }
+        }
+    `;
+
+    try {
+        const result = await request(graphqlAPI, query);
+        return result.posts;
+    } catch (error) {
+        console.error('Error fetching recent posts:', error);
+        return [];
+    }
 };
